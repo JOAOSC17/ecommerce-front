c436236d797a5baa3b66eaf2c46806d184d11721
João Costa <costav1402@gmail.com>
2021-12-10 18:32:35 -0300

    create navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import Announcement from '../components/Announcement'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
     return (
     <div>
+        <Announcement/>
         <Navbar/>
     </div>
     )
