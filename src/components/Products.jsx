c48891b8fd13c719120ce5adcd16664938d539e5
João Costa <costav1402@gmail.com>
2021-12-13 14:39:24 -0300

    crete categories section

---
+++ src/components/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+import {categories} from "../data"
+
+
+export default function Products() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,7 +6,6 @@ const Container = styled.div`
     width:100%;
     height:100vh;
     display:flex;
-    background-color: coral;
     position:relative;
     overflow:hidden;
 `
