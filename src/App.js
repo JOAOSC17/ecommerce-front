b39eb2e0accdde2129394ac22f9e67182da0b4cf
João Costa <costav1402@gmail.com>
2021-12-14 10:21:17 -0300

    create login and register page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <div className="App">
-     <Product />
+     <Register />
     </div>
   );
 }
